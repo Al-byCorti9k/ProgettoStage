cab3f022fb0bd7ce4a2f7006fa765a5efb800c57
AlbertoCorti <a.corti62@campus.unimib.it>
2025-11-10 20:15:31 +0100

    implementazione di LOOCV e MCC + esercizio esempio

--- pythonVersion/bin/Main.py
+++ pythonVersion/bin/Main.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
-from sklearn import linear_model
+from sklearn import linear_model, model_selection
+from sklearn.metrics import matthews_corrcoef
+
 import array
 #ottengo il percorso del file Main.py
 p = pathlib.Path(__file__)
@@ -15,13 +17,37 @@ df = pd.read_csv(p_csv)
 print(df.tail(5))
 
 #ottengo tutte le colonne
-df_columns = df.columns[:-1].tolist()
+x_predictor = df[df.columns[:-1]]
+#print(x_predictor.shape)
+y_response = df[df.columns[-1]]
+#print(y_response.shape)
 
 
-reg = linear_model.LinearRegression()
-# le doppie [ servono a creare un dataframe a partire da un altro
-reg.fit(df[df_columns], df.outcome)
+#sezione aggiornata
 
-#test
-print(reg.predict(df.iloc[[-1], :-1]))
-print(reg.predict(df.iloc[[-4], :-1]))
+#obiettivo: leave-one-out cross validation per verificare che il modello
+#abbia una capacità di generalizzazione solida. 
+
+
+
+#importiamo il modello sulla quale si baserà l'algoritmo di machine learning
+#NOTA al momento uso la logisticregression, di modo che i dati siano binari.
+# questo ha dei problemi di performance perchè l'algoritmo potrebbe non finire 
+# ben precisato di passi. 
+model = linear_model.LogisticRegression(max_iter = 1000)
+
+#indichiamo il metodo che useremo per validare le capacità di generalizzazione
+cvp = model_selection.LeaveOneOut()
+
+#possiamo avviare la validazione. Ho scelto la funzione cross_val_predict.
+# rispetto alle altre due alternative (cross_val, cross_val_score) mi permette
+# di ricavare comodamente tutti i valori delle predizioni nei singoli fold.
+# tornerà utile per il calcolo dell'MCC
+
+y_predict = model_selection.cross_val_predict(model, x_predictor, y_response, cv = cvp )
+print(y_predict)
+
+MCC = matthews_corrcoef(y_response, y_predict)
+print(MCC)
+
+# MCC = 0.46382761282805135 bassa correlazione
