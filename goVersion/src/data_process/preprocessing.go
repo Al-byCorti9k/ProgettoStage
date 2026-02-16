4d352a0a7d58194248b6ad7acc5733ca37f47810
AlbertoCorti <a.corti62@campus.unimib.it>
2026-02-16 19:20:42 +0100

    go version - testate le funzioni per il riempimento con la media e la moda dei valori Nan

--- goVersion/src/data_process/preprocessing.go
+++ goVersion/src/data_process/preprocessing.go
@@ -1,1 +1,148 @@
 package dataprocess
+
+import (
+	//"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
+	//"fmt"
+	//"log"
+	"math"
+)
+
+// funzione che gestice i valori vuoti di un dataframe
+func FillColumnsNanValues(dfInfo *DataframeInfo) DataframeInfo {
+
+	df := fillNanWithMode(dfInfo)
+
+	newDfInfo := DataframeInfoBuild(dfInfo.Id, &df.Df)
+
+	df2 := fillNanWithMean(&newDfInfo)
+
+	finalDfInfo := DataframeInfoBuild(newDfInfo.Id, &df2.Df)
+
+	return finalDfInfo
+}
+
+func fillNanWithMean(dfInfo *DataframeInfo) DataframeInfo {
+
+	colsInfo, _ := GetDatasetInfo(&dfInfo.Id)
+	catCols := colsInfo.VecToHashSet()
+	//prendo tutte le colonne che non sono categoriche
+	allCols := dfInfo.Df.Names()
+
+	// Inizializziamo dfWorking con il dataframe originale
+	dfWorking := dfInfo.Df
+	//nel caso nessuna colonna avesse valori nulli
+	i := 0
+
+	for _, col := range allCols {
+		colToCheck := dfWorking.Col(col)
+		//per capire se sia categorica o meno
+		_, exists := catCols[col]
+		if colToCheck.HasNaN() && !exists {
+
+			meanVal := calculateMean(&colToCheck)
+			//modifica il dataframe originale con la nuova colonna
+			dfWorking = dfWorking.Mutate(
+				series.New(fillMissingValues(&colToCheck, meanVal), series.Float, col))
+			i++
+		}
+	}
+	if i == 0 {
+		return *dfInfo
+	}
+	//alla fine ricostruisco un nuovo dataframe
+	newDfInfo := DataframeInfoBuild(dfInfo.Id, &dfWorking)
+
+	return newDfInfo
+}
+
+// calcola la media
+func calculateMean(s *series.Series) float64 {
+	if s.Len() == 0 {
+		return 0.0
+	}
+
+	var sum float64
+	var count int
+
+	for i := 0; i < s.Len(); i++ {
+		val := s.Elem(i).Float()
+
+		// Controlliamo se il valore è un numero valido
+		if !math.IsNaN(val) {
+			sum += val
+			count++
+		}
+	}
+
+	// Se tutti i valori erano NaN, evitiamo la divisione per zero
+	if count == 0 {
+		return 0.0
+	}
+
+	return sum / float64(count)
+}
+
+// riempie le colonne categoriche con la moda
+func fillNanWithMode(dfInfo *DataframeInfo) DataframeInfo {
+
+	cols, _ := GetDatasetInfo(&dfInfo.Id)
+	catCols := cols.VecToHashSet()
+	// Inizializziamo dfWorking con il dataframe originale
+	dfWorking := dfInfo.Df
+	//nel caso nessuna colonna avesse valori nulli
+	i := 0
+
+	for catCol, _ := range catCols {
+		colToCheck := dfWorking.Col(catCol)
+		if colToCheck.HasNaN() {
+			modeVal := calculateMode(&colToCheck)
+			//modifica il dataframe originale con la nuova colonna
+			dfWorking = dfWorking.Mutate(
+				series.New(fillMissingValues(&colToCheck, modeVal), series.Float, catCol))
+			i++
+		}
+	}
+	if i == 0 {
+		return *dfInfo
+	}
+	//alla fine ricostruisco un nuovo dataframe
+	newDfInfo := DataframeInfoBuild(dfInfo.Id, &dfWorking)
+
+	return newDfInfo
+}
+
+// Funzione di supporto per calcolare la moda in una Serie di Gota
+func calculateMode(s *series.Series) float64 {
+	counts := make(map[float64]int)
+	maxFreq := 0
+	var mode float64
+
+	// Iteriamo sugli elementi della colonna
+	for i := 0; i < s.Len(); i++ {
+		val := s.Elem(i).Float()
+
+		counts[val]++
+
+		if counts[val] > maxFreq {
+			maxFreq = counts[val]
+			mode = val
+		}
+	}
+
+	return mode
+}
+
+// riempie i valori nulli delle series con i valori che vuoi.
+func fillMissingValues(s *series.Series, replacement float64) []float64 {
+	// Estraiamo i valori come float64
+	vals := s.Float()
+
+	for i, v := range vals {
+		// In Go, math.IsNaN è il modo standard per controllare i valori mancanti nei float
+		if math.IsNaN(v) {
+			vals[i] = replacement
+		}
+	}
+	return vals
+}
--- goVersion/src/main.go
+++ goVersion/src/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 
+	//"os"
+
 	dataprocess "github.com/Al-byCorti9k/ProgettoStage/goVersion/src/data_process"
 
 	cli "github.com/Al-byCorti9k/ProgettoStage/goVersion/src/cli_tools"
@@ -22,12 +24,38 @@ func main() {
 	df := dataprocess.GetDataframeFromID(args.DArgs[0])
 
 	dfInfo := dataprocess.DataframeInfoBuild(args.DArgs[0], &df)
-	println(args.CArgs[0])
-	cli.InputColumnsCheck(&dfInfo, &args.CArgs[0])
+
+	var targetColumn string
+
+	if len(args.CArgs) != 0 {
+		cli.InputColumnsCheck(&dfInfo, &args.CArgs[0])
+		targetColumn = args.CArgs[0]
+	} else {
+		_, numberCols := dfInfo.Df.Dims()
+		targetColumn = dfInfo.Df.Names()[numberCols-1]
+	}
+	println(targetColumn)
 
 	fmt.Println(dfInfo)
+
+	dfInfo2 := dataprocess.FillColumnsNanValues(&dfInfo)
+
+	fmt.Println(dfInfo2)
 
 	//fmt.Println(a.VecToHashSet())
 	fmt.Println(dfInfo.Df.Names())
 
+	/*
+		f, err := os.Create("output.csv")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+
+		// 3. Esportiamo il DataFrame nel file
+		err = dfInfo2.Df.WriteCSV(f)
+		if err != nil {
+			log.Fatal(err)
+		}
+	*/
 }
